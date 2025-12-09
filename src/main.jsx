7a9107532d43f79e09cf0350fd5294f9b36605e5
ashadulislam6156rs <ashadulislam6156rs@gamil.com>
2025-12-09 12:08:59 +0600

    added database and fetch data for using tanstack query

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,16 @@ import './index.css'
 import { RouterProvider } from 'react-router'
 import router from './Routers/Router'
 import AuthProvider from './AuthProvider/AuthProvider'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthProvider>
+    <QueryClientProvider client={queryClient}>
+       <AuthProvider>
       <RouterProvider router={router}></RouterProvider>
     </AuthProvider>
+    </QueryClientProvider>
+   
   </StrictMode>
 );
